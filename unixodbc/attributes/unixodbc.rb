18492ae67be3093ea86c0474644c1b9e777b45b4
Darrin Eden <darrin.eden@gmail.com>
2010-04-01 22:22:29 -0700

    unixODBC cookbook

---
+++ unixodbc/attributes/unixodbc.rb
@@ -0,0 +1,2 @@
+set_unless[:unixodbc][:version] = "2.2.14-p2"
+set_unless[:unixodbc][:checksum] = "41d04978"
---
+++ unixodbc/recipes/default.rb
@@ -0,0 +1,34 @@
+version = node[:unixodbc][:version]
+
+remote_file "/usr/src/unixODBC-#{version}.tar.gz" do
+  source "http://www.unixodbc.org/unixODBC-#{version}.tar.gz"
+  checksum node[:unixodbc][:checksum]
+end
+
+execute "untar unixODBC" do
+  command "tar xzf unixODBC-#{version}.tar.gz"
+  cwd "/usr/src"
+  user "root"
+  group "root"
+  creates "/usr/src/unixODBC-#{version}/README"
+end
+
+execute "configure unixODBC" do
+  command "./configure --prefix=/usr --sysconfdir=/etc --enable-gui=no"
+  cwd "/usr/src/unixODBC-#{version}"
+  creates "/usr/src/unixODBC-#{version}/config.log"
+end
+
+execute "build unixODBC" do
+  command "make"
+  cwd "/usr/src/unixODBC-#{version}"
+  creates "/usr/src/unixODBC-#{version}/exe/isql"
+end
+
+execute "install unixODBC" do
+  command "make install"
+  cwd "/usr/src/unixODBC-#{version}"
+  user "root"
+  group "root"
+  creates "/usr/lib/libodbc.so"
+end
