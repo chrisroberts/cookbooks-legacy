25186ad135dc4034275e684bd053d1c009aae0c0
dje <darrin.eden@gmail.com>
2010-04-26 15:25:45 -0600

    more mongrel+monit attributes. should be moved into mongrel attributes.

--- monit/attributes/monit.rb
+++ monit/attributes/monit.rb
@@ -1,3 +1,6 @@
 set_unless[:monit][:email] = "darrin@heavywater.ca"
-set_unless[:monit][:start_port] = "5000"
-set_unless[:monit][:servers] = "5"
+set_unless[:monit][:start_port] = 5000 
+set_unless[:monit][:servers] = 5
+set_unless[:monit][:total_memory] = 225
+set_unless[:monit][:mongrel_bin_path] = "/usr/bin/mongrel_rails"
+set_unless[:monit][:mongrel_config_path] = "/etc/mongrel/app.yml"
--- monit/recipes/mongrel.rb
+++ monit/recipes/mongrel.rb
@@ -5,11 +5,14 @@ template "/etc/monit/monitrc" do
   owner "root"
   group "root"
   mode "600"
-  only_if "test -f /etc/mongrel/app.yml"
+  only_if "test -f #{node[:monit][:mongrel_config_path]}"
   end_port = node[:monit][:start_port].to_i + node[:monit][:servers].to_i - 1
   variables(
     :start_port => node[:monit][:start_port].to_i,
     :end_port => end_port,
-    :email => node[:monit][:email])
+    :email => node[:monit][:email],
+    :total_memory => node[:monit][:total_memory],
+    :bin_path => node[:monit][:mongrel_bin_path],
+    :config_path => node[:monit][:mongrel_config_path] )
   notifies :reload, resources(:service => "monit")
 end
