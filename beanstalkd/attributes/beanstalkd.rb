06594093beb256aa356e9c465044525c62ad1d9f
dje <darrin.eden@gmail.com>
2010-06-06 16:30:59 -0700

    beanstalkd is in lucid. simplify.

--- beanstalkd/attributes/beanstalkd.rb
+++
@@ -1,2 +0,0 @@
-set_unless[:beanstalkd][:version] = "1.4.3"
-set_unless[:beanstalkd][:checksum] = "f6bb5f50"
--- beanstalkd/recipes/ruby.rb
+++
@@ -1,7 +0,0 @@
-include_recipe "beanstalkd"
-
-gem_package "beanstalk-client" do
-  path = node[:rvm][:gem_binary]
-  gem_binary path if path
-  action :upgrade
-end
