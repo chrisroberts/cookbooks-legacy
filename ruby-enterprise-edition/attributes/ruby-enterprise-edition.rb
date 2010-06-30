dd17e2a59ccc3336d928d03175e14dc2b1ebc09e
Darrin Eden <darrin.eden@gmail.com>
2010-06-30 09:57:05 -0700

    update ruby enterprise edition to current release

--- ruby-enterprise-edition/attributes/ruby-enterprise-edition.rb
+++ ruby-enterprise-edition/attributes/ruby-enterprise-edition.rb
@@ -1,11 +1,11 @@
 if kernel[:machine] == "x86_64"
-  deb = "ruby-enterprise_1.8.7-2010.01_amd64.deb"
-  set_unless[:ree][:project_url] = "http://rubyforge.org/frs/download.php/68720/#{deb}"
-  set_unless[:ree][:checksum] = "2e682eb9"
+  deb = "ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb"
+  default[:ree][:project_url] = "http://rubyforge.org/frs/download.php/71098/#{deb}"
+  default[:ree][:checksum] = "583c085d"
 else
-  deb = "ruby-enterprise_1.8.7-2010.01_i386.deb"
-  set_unless[:ree][:project_url] = "http://rubyforge.org/frs/download.php/68718/#{deb}"
-  set_unless[:ree][:checksum] = "0465bd70"
+  deb = "ruby-enterprise_1.8.7-2010.02_i386_ubuntu10.04.deb"
+  default[:ree][:project_url] = "http://rubyforge.org/frs/download.php/71100/#{deb}"
+  default[:ree][:checksum] = "39f01e42"
 end
 
-set_unless[:ree][:deb] = deb
+default[:ree][:deb] = deb
