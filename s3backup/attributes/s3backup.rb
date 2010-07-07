7beedc5bfac043a311d1be77c3b5faee6e841bb1
dje <darrin.eden@gmail.com>
2010-07-07 11:54:03 -0700

    initial pass at an s3 backup cookbook

---
+++ s3backup/attributes/s3backup.rb
@@ -0,0 +1,4 @@
+default[:aws][:key_id] = "aws key id"
+default[:aws][:secret] = "aws secret"
+default[:s3backup][:postgres_bucket] = "bucket_name"
+default[:s3backup][:dump_directory] = "/tmp/postgres_dumps"
---
+++ s3backup/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Heavy Water Software Inc."
+maintainer_email  "darrin@heavywater.ca"
+license           "MIT"
+description       "Installs S3 backup scripts"
+
+supports "ubuntu"
