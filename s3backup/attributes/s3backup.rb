b5a2b6e65fbad26c55dd806ddef76afd0e6bfcd0
Darrin Eden <darrin.eden@gmail.com>
2010-07-08 09:42:02 -0700

    smoothin' it out

--- s3backup/attributes/s3backup.rb
+++ s3backup/attributes/s3backup.rb
@@ -1,4 +1,7 @@
 default[:aws][:key_id] = "aws key id"
 default[:aws][:secret] = "aws secret"
-default[:s3backup][:postgres_bucket] = "bucket_name"
-default[:s3backup][:dump_directory] = "/tmp/postgres_dumps"
+default[:s3backup][:keep_days] = 8
+default[:s3backup][:minute] = 0
+default[:s3backup][:hour] = 22
+default[:s3backup][:postgres_bucket] = "bucket-name"
+default[:s3backup][:dump_directory] = "/var/backups/postgresql"
--- s3backup/recipes/postgres.rb
+++ s3backup/recipes/postgres.rb
@@ -19,6 +19,19 @@ template "/usr/local/sbin/s3_postgres_backup.sh" do
   variables( :key_id => node.aws.key_id,
              :secret => node.aws.secret,
              :bucket => node.s3backup.postgres_bucket,
+             :keep_days => node.s3backup.keep_days,
              :dump_directory => node.s3backup.dump_directory )
 end
 
+directory "postgresql backup" do
+  path node.s3backup.dump_directory
+  owner "postgres"
+  group "postgres"
+end
+
+cron "postgres backup cron job" do
+  minute node.s3backup.minute
+  hour node.s3backup.hour
+  command "/usr/local/sbin/s3_postgres_backup.sh"
+  user "postgres"
+end
