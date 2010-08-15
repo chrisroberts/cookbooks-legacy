000a1238a0d0abd48e05814763e0ab3a304ae66d
Darrin Eden <darrin.eden@gmail.com>
2010-08-15 16:14:03 -0700

    simplify java cookbook

--- java/recipes/default.rb
+++ java/recipes/default.rb
@@ -1,8 +1,1 @@
-package "openjdk-6-jdk" do
-  action :upgrade
-end
-
-execute "set JAVA_HOME" do
-  command "echo JAVA_HOME=/usr >> /etc/environment"
-  not_if "grep JAVA_HOME /etc/environment"
-end
+package "openjdk-6-jdk"
