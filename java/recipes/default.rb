29989cbb7c1b1c868c7152d8288061dcef7977e4
Darrin Eden <darrin.eden@gmail.com>
2010-08-28 12:41:35 -0700

    switched to sun jdk

--- java/recipes/default.rb
+++ java/recipes/default.rb
@@ -1,1 +1,19 @@
-package "openjdk-6-jdk"
+execute "synchronize package index" do
+  command "apt-get update"
+  action :nothing
+end
+
+execute "set java alternatives" do
+  command "update-java-alternatives -s java-6-sun"
+  returns 2
+  action :nothing
+end
+
+template "/etc/apt/sources.list.d/lucid_partners" do
+  notifies :run, resources(:execute => "synchronize package index"), :immediately
+end
+
+package "sun-java6-jdk" do
+  response_file "java.seed"
+  notifies :run, resources(:execute => "set java alternatives"), :immediately
+end
