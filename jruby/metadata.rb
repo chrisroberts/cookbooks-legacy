0a902e32aebc959c2e70de468c177a5f4d5f38ba
Darrin Eden <darrin.eden@gmail.com>
2010-08-28 12:50:58 -0700

    use jruby via rvm instead

--- jruby/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer        "Heavy Water Software, Inc."
-maintainer_email  "darrin@heavywater.ca"
-license           "MIT"
-description       "Installs jruby"
-
-%w{ java ant git }.each do |d|
-  depends d
-end
